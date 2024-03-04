faa21bcb02bfed99f71095974c7732874f0e488a
easy <159019628+cvcasyak@users.noreply.github.com>
2024-03-04 15:10:39 +0300

    Add files via upload

---
+++ easy.h
@@ -0,0 +1,9 @@
+#ifndef EASY_H_INCLUDED
+#define EASY_H_INCLUDED
+
+void itc_name();
+void itc_fio();
+int itc_abc();
+double itc_fabs();
+int itc_revnbr();
+#endif // EASY_H_INCLUDED
---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+itc_name();
+itc_fio;
+itc_abc();
+itc_fabs();
+itc_revnbr();
+    return 0;
+}
