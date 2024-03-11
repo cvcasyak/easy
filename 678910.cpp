31a714a710e7bcdb0c6ff5667d4cfb415daa7389
easy <159019628+cvcasyak@users.noreply.github.com>
2024-03-11 15:14:44 +0300

    Add files via upload

---
+++ 678910.cpp
@@ -0,0 +1,12 @@
+#include "easy.h"
+#include <iostream>
+using namespace std;
+
+bool itc_iseven(int num){
+      if (num % 2 == 0){
+        return True;
+      }
+      else{
+        return False;
+    }
+}
